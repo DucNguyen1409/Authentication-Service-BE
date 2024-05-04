928222c83b58d49c62d5534feb701d1a52087705
nguyenduc <1409.ducnguyen@gmail.com>
2024-05-04 14:36:04 +0700

    [04/05] Update application yml file

--- src/main/java/com/example/spring/authentication/config/ApplicationConfig.java
+++ src/main/java/com/example/spring/authentication/config/ApplicationConfig.java
@@ -2,10 +2,9 @@ package com.example.spring.authentication.config;
 
 import com.example.spring.authentication.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -18,13 +17,14 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-    private static final String FE_END_POINT = "http://localhost:4200";
+
+    @Value("${spring.cors.origins:*}")
+    private List<String> AllowedOrigins;
     private final UserRepository userRepository;
 
     @Bean
@@ -56,12 +56,14 @@ public class ApplicationConfig {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Collections.singletonList(FE_END_POINT));
-        configuration.setAllowedHeaders(List.of(HttpHeaders.ORIGIN, HttpHeaders.CONTENT_TYPE,
-                                                HttpHeaders.ACCEPT, HttpHeaders.AUTHORIZATION));
-        configuration.setAllowedMethods(List.of(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-                                                HttpMethod.DELETE.name(), HttpMethod.PATCH.name()));
+//        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(AllowedOrigins);
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+//        configuration.setAllowedHeaders(List.of(HttpHeaders.ORIGIN, HttpHeaders.CONTENT_TYPE,
+//                                                HttpHeaders.ACCEPT, HttpHeaders.AUTHORIZATION));
+//        configuration.setAllowedMethods(List.of(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+//                                                HttpMethod.DELETE.name(), HttpMethod.PATCH.name()));
         source.registerCorsConfiguration("/**", configuration);
 
         return new CorsFilter(source);
